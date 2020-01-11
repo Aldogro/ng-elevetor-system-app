7f2d57e2a8c189f6519f965d5fc6c7645b4cb11f
Aldogro <aldogrojas5@gmail.com>
2020-01-11 11:38:19 -0300

    removed unused code, generated common components

---
+++ src/app/components/building/building.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-building',
+  templateUrl: './building.component.html',
+  styleUrls: ['./building.component.sass']
+})
+export class BuildingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/command/command.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-command',
+  templateUrl: './command.component.html',
+  styleUrls: ['./command.component.sass']
+})
+export class CommandComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
